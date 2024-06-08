bae9b7e52cfbec86dc2e5c4f9ba0f44d5771f940
mgomes-d <murillo.jogos8@hotmail.com>
2024-06-08 20:43:49 +0200

    basic redirection done

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
+
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
     return 'This is my portfolio'
 
+@app.route('/success/<int:score>')
+def success(score):
+    return "GG"
+
+@app.route('/fail/<int:score>')
+def fail(score):
+    return "RIP"
+
+@app.route('/result/<int:mark>')
+def result(mark):
+    result = ""
+    if mark < 50:
+        result = "fail"
+    else:
+        result = "success"
+    return redirect(url_for(result, score=mark))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
