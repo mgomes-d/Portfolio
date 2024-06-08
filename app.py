648c34d6b3994e8e884862c8d876346d6e58daf3
mgomes-d <murillo.jogos8@hotmail.com>
2024-06-08 21:24:50 +0200

    basic integration of html

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 
-app=Flask(__name__)
+app=Flask(__name__) 
 
 @app.route('/')
 def welcome():
-    return 'This is my portfolio'
+    return render_template('index.html')
 
-@app.route('/success/<int:score>')
-def success(score):
-    return "GG"
-
-@app.route('/fail/<int:score>')
-def fail(score):
-    return "RIP"
-
-@app.route('/result/<int:mark>')
-def result(mark):
-    result = ""
-    if mark < 50:
-        result = "fail"
+@app.route('/result/<int:score>')
+def result(score):
+    res=""
+    if score >= 50:
+        res = "PASS"
     else:
-        result = "success"
-    return redirect(url_for(result, score=mark))
+        res = "FAIL"
+    return render_template('result.html', result=res)
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    total_score = 0
+    if request.method == 'POST':
+        data_science = float(request.form['datascience'])
+        total_score = data_science
+    return redirect(url_for("result", score=total_score))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
